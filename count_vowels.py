cc3b878ffd20df7c57d8d90eddd8e7d84a0ef94a
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-03 11:35:26 +0300

    checks for lowercase

--- count_vowels.py
+++ count_vowels.py
@@ -1,6 +1,6 @@
 def count_vowels(text: str) -> int:
     vowels = ["a", "e", "i", "o", "u"]
-    vowel_counts = [vowel for vowel in text if vowel in vowels]
+    vowel_counts = [vowel for vowel in text.lower() if vowel in vowels]
     return len(vowel_counts)
 
 
